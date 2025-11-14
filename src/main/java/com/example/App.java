312366344668bcdc48d090ccd4cde24b881d03ba
cloudTechSneha <saisnehacloud23@gmail.com>
2025-11-14 10:35:28 +0530

    Create App.java

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class App {
+    public static String greet(String name) {
+        if (name == null) return "Hello, world!";
+        return "Hello, " + name + "!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(greet(args.length > 0 ? args[0] : null));
+    }
+}
